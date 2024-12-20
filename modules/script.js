57f18d39dbdd56082bc5f20684c684fd14502368
ktectournament <usera.akame@gmail.com>
2024-12-20 15:20:37 +0900

    Update script.js

--- modules/script.js
+++ modules/script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
         <table><tr><td>アッパーブラケット(UB)</td><td>12/19 16:30</td></tr><tr><td>ロウワーブラケット(LB)</td><td>12/19 16:30</td></tr><tr><td>グランドファイナル(GF)</td><td>12/20 16:30</td></tr></table>
         <p>エントリーフォーム: <a href="https://forms.gle/AZq2ioSZoT34LNx38" target="_blank">ここをクリック</a></p>
         <p>エントリー可能期間: 12/19 ~13:00迄<p>
-        <p>トーナメント表:<br><a href="https://the-tournament.net/tournaments/Iw5tRyLUHx79KXWhfIX5/share" target="_blank">アッパーブラケット</a><br><a href="https://the-tournament.net/tournaments/pGv72GEe1vKISGkfy18d/share" target="_blank">ロウワーブラケット</a></p>
+        <p>トーナメント表:<br><a href="https://the-tournament.net/tournaments/Iw5tRyLUHx79KXWhfIX5/share" target="_blank">アッパーブラケット</a><br><a href="https://the-tournament.net/tournaments/pGv72GEe1vKISGkfy18d/share" target="_blank">ロウワーブラケット</a><br><a href="https://the-tournament.net/tournaments/bupIEKYK0MT6BDLB0JQU/share" target="_blank">グランドファイナル</a><br><div style='position:relative!important;width:100%!important;max-width:100%!important;overflow:auto !important;-webkit-overflow-scrolling:touch !important;'><iframe src='https://the-tournament.net/tournaments/bupIEKYK0MT6BDLB0JQU/embed' seamless frameborder='0' scrolling='no' width='100%' height='364' style='width:1px;min-width:100%;'></iframe></div></p>
         <p>結果: 後日公開</p>
         <p>大会キャンセルは<a href="https://forms.gle/rN4eMGx5sH821J3Z9" target="_blank">こちら</a></p>    
         `,
